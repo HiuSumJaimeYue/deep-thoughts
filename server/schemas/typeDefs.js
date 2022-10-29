54f2ec70bffb92d9d086e0abcaee30efd1e9ac14
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-29 19:49:54 -0400

    Add mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,7 +11,13 @@ type User {
     thoughts: [Thought]
     friends: [User]
   }
-type Thought {
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
@@ -28,10 +34,19 @@ type Thought {
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
