830c9c202915c8200bda3c9ed0370bc306f8538e
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-31 04:43:01 -0400

    Add production only code

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,10 +7,6 @@ const mongoose = require('mongoose');
 //   useFindAndModify: false
 // });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
   {
